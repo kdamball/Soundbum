22180772f589115b50883d7d34e9b52143d353ea
Kado <kadodamball@hotmail.com>
2013-12-24 19:24:01 +0300

    Removed the autoplay feature until setTimeout is sorted

--- snd.js
+++ snd.js
@@ -1,5 +1,6 @@
 (function(){
   $('form').on('submit', function(){
+  
     var myGenre = $('#genre').val();
     //for errors
     function remove(message){
@@ -9,14 +10,14 @@
         $('form').append('<span style="color:red"><br>' +message+'</span>')
       }
     }
-    
+      
     if (myGenre == null || myGenre.length == 0){
       remove("You didn't enter any genre");
     }
     
     else if (myGenre.length >= 1){
       SC.initialize({
-        client_id: 'client_id_goes_here'
+        client_id: '762b8d030947ba97c00769ffb6c5e61e'
       });
       
       SC.get(
@@ -27,7 +28,7 @@
         function(tracks, error) {
           var random = Math.floor(Math.random() * tracks.length);
           if (error){
-            remove("Couldn't find any songs, sorry!");
+            remove("Sorry, couldn't find any songs under " +myGenre);
             document.title = "No Song Found";
             return;
           }else{
@@ -37,17 +38,29 @@
             
             SC.oEmbed(
               track_url, 
-              {auto_play: true, color: "#ff0066"},
+              {
+                auto_play: true, 
+                color: "#ff0066", 
+                show_comments: false, 
+                download:true,
+                show_artwork:true
+              },
               $("#target")[0]
             );
+            
+            if(tracks[random].description){
+              $('#info').empty().append("Song Description: " +tracks[random].description);
+            }
+
             //just for the sake of it (future feature??)
             var hist = {foo: SC.oEmbed(track_url, {auto_play: true, color: "#ff0066"})};
             history.pushState(hist, tracks[random].title, ""); 
             
           }
-        
+          window.time = tracks[random].duration + 10000;
         }
       );
     }
+      
   });
-})()
+})();
