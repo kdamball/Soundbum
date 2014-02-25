bb9461e834b3340abffc8be95e56dd42d82e00df
Kado <kadodamball@hotmail.com>
2014-02-25 13:03:11 +0300

    For some reason my last night commits don't appear

--- js/snd.js
+++ js/snd.js
@@ -2,8 +2,16 @@
 
 var Snd = (function(){
   
+  var myGenre = $("#genre").val();
+  
+  var displayError = function(message){
+    var message = message.toString();
+    $('form').append('<span style="color:red"><br>' +message+'</span>')
+  }
+  
   return {
     remove: dispose,
+    error: diplayError,
     play: load
   }
   
