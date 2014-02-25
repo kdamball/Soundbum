5dbc5fdf526d8f3c02b2cf21772e122425e0d9de
kdamball <kadodamball@hotmail.com>
2014-02-25 14:43:33 +0300

    Refactoring the entire code

--- js/snd.js
+++ js/snd.js
@@ -2,21 +2,76 @@
 
 var Snd = (function(){
   
-  var errorMessages = [
-    "No song found under "+myGenre,
-    "You didn't enter any genre"
-  ];
+  self = this;
   
-  var displayError = function(message){
+  
+  
+  var displayError = function(errorCode){
+    var errorMessages = [
+      "No song found under "+myGenre,
+      "You didn't enter any genre"
+    ];
     var message = message.toString();
-    $('form').append(error)
+    $('#error').append(errorMessage[errorCode])
   };
   
   var dispose = function(element){
     $(element).empty()
   }
   
+  var widgetSetup = function(){
+    var iframe = $('iframe')[0], widget = SC.Widget(iframe);
+    widget.bind(SC.Widget.Events.READY, function(){
+      widget.bind(SC.Widget.Events.FINISH, function(){
+        self.play()
+      });
+    });
+  };
+  
   var load = function(){
+    SC.initialize({
+      client_id: '762b8d030947ba97c00769ffb6c5e61e'
+    });
+    
+    SC.get(
+        '/tracks', 
+        
+        { genres: myGenre },
+        
+        function(tracks, error) {
+
+          if (error){
+          
+            self.error(0);
+            
+          }else{
+            
+            var random = Math.floor(Math.random() * tracks.length);
+            self.remove("#error")
+            document.title = tracks[random].genre + " : " + tracks[random].title ;
+            var track_url = tracks[random].permalink_url;
+            SC.oEmbed(
+              track_url, 
+              {
+                auto_play: true, 
+                color: "#ff0066", 
+                show_comments: false, 
+                download:true,
+                show_artwork:true
+              },
+              $("#target")[0]
+            );
+            
+            if(tracks[random].description){
+              $('#info').empty().append("Song Description: " +tracks[random].description);
+            }
+            
+            $('iframe').attr('sandbox', 'allow-same-origin');
+            
+            setTimeout(widgetSetup, 2000);
+            
+          }
+      });
     
   };
   
@@ -28,6 +83,16 @@ var Snd = (function(){
   
 })();
 
+
 (function(){
-  $("form").on("submit", app.getSong)
+  $("form").on("submit", function(){
+  
+    var myGenre = $("#genre").val();
+    
+    if(myGenre == null || myGenre.length< 0){
+      app.error(1);
+    }else{
+      app.play();
+    }
+  });
 })();
