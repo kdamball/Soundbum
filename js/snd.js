4f52c04397730bf56d2f9f6c37fc9c49f524bab3
kdamball <kadodamball@hotmail.com>
2014-02-26 13:49:06 +0300

    Minor appearance changes

--- js/snd.js
+++ js/snd.js
@@ -65,7 +65,7 @@ var Snd = (function(myGenre){
             );
             
             if(tracks[random].description){
-              $('#info').empty().append("Song Description: " +tracks[random].description);
+              $('#info').empty().append("<strong>Song Description:</strong> " +tracks[random].description);
             }
             
             setTimeout(widgetSetup, 2000);
